20f146dccc940ca0bf692549e2e3da482460966a
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-05 01:44:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,18 +9,3 @@ class Person
     @name
   end
 end
-
-class Person
-  def initialize(name)
-    @name=name
-  end
-  def initialize(breed=Mutt)
-    @breed=breed
-  end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
-end
